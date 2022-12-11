52b2baa3599221360c43ab69781783de51a9d840
Aditya Malik Gupta <76451125+adityamalikgupta7755@users.noreply.github.com>
2022-12-11 07:24:12 -0800

    aa

--- CMS3/settings.py
+++ CMS3/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-15gl65h6k8kwh)+=d2^pgf90qrk&sm_dv*tg)qa%@h+i@0d8^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','cmsv1-production.up.railway.app']
+ALLOWED_HOSTS = ['cmsv1-production.up.railway.app']
 
 
 # Application definition
