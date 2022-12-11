e9ce30a59075f444f351728fdd7c07be4ed1afff
Aditya Malik Gupta <76451125+adityamalikgupta7755@users.noreply.github.com>
2022-12-11 07:36:24 -0800

    se4

--- CMS3/settings.py
+++ CMS3/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15gl65h6k8kwh)+=d2^pgf90qrk&sm_dv*tg)qa%@h+i@0d8^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["cmsv1-production.up.railway.app","chrome-extension://theIDofYourExtension"]
 
