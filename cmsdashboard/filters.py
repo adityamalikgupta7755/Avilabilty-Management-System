5452dba2c3fc1bb4a54f9b83546cdfb2e2814f89
Aditya Malik Gupta <76451125+adityamalikgupta7755@users.noreply.github.com>
2022-12-11 06:58:32 -0800

    fil

--- cmsdashboard/filters.py
+++ cmsdashboard/filters.py
@@ -1,16 +1,13 @@
 from cProfile import label
 from cgitb import lookup
 from dataclasses import field
-from tkinter.tix import Select
 import django_filters
-from .models import emp,emp_status
+from .models import emp,
 from django_filters import CharFilter
-from django import forms
-from django.forms import ModelForm
+
 
 class empfilter(django_filters.FilterSet):
     emp_name = CharFilter(field_name='emp_name', lookup_expr='icontains', label='')
-    # emp_shift = Select(field_name='emp_shift', lookup_expr='icontains', label='')
     class Meta:
         model = emp
         fields ={'emp_name','emp_shift'}
@@ -19,8 +16,3 @@ class empfilter(django_filters.FilterSet):
 
         exclude=['emp_id']
 
-
-# class emp_statusfilter(django_filters.FilterSet):
-#     class Meta:
-#         model = emp_status
-#         fields =['emp']
