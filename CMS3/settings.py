47ae32c812b8b1f9a2ac3b6c983f506f920a26b1
Aditya Malik Gupta <76451125+adityamalikgupta7755@users.noreply.github.com>
2022-12-11 08:13:35 -0800

    .
    .

--- CMS3/settings.py
+++ CMS3/settings.py
@@ -146,14 +146,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/image/'
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
-]
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_ROOT =os.path.join(BASE_DIR, 'static/image')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
